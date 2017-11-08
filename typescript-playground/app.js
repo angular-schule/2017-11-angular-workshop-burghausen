12281adda350dfdf17828454cebfca224c58a7d2
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-08 11:32:12 +0100

    upsi! please treat *.js.* files as build artifacts!

--- typescript-playground/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var test_1 = require("./test");
-var test = new test_1.Test(42);
-//test.id = "xxx" 
-//# sourceMappingURL=app.js.map
