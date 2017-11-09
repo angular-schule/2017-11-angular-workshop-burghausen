0ffbc54a63da05ab69bab0a38ecaf598d680703f
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-09 15:14:58 +0100

    subscribing with @ViewChild

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,22 @@
+import { BookComponent } from './../book/book.component';
+
 import { Book } from './../shared/book';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent
+  implements OnInit, AfterViewInit, OnDestroy {
 
   books: Book[];
+
+  @ViewChild(BookComponent)
+  bookComponent: BookComponent;
+
+  subscription: any;
 
   constructor() { }
 
@@ -17,10 +25,20 @@ export class DashboardComponent implements OnInit {
       new Book('000', 'Angular', 'Einstieg in die komponentenbasierte Entwicklung...', 5),
       new Book('111', 'AngularJS', 'Einstieg in das Framework')
     ];
+    this.reorderBooks(null);
+  }
+
+  ngAfterViewInit() {
+    this.subscription = this.bookComponent.rated
+      .subscribe((book) => console.log('RxJS!', book));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   reorderBooks(book: Book) {
-    console.log(book);
+    //console.log(book);
     this.books.sort((a, b) => b.rating - a.rating);
   }
 }
