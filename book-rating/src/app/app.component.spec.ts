d0f173c4cf60ab579e833f47a1f073e4d28ac94a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-09 15:42:59 +0100

    this is now a Shallow Unit Test

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,7 +1,10 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-xdescribe('AppComponent', () => {
+
+describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -10,22 +13,27 @@ xdescribe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA] // <!-- this is now a Shallow Unit Test
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'br'`, async(() => {
+
+  it(`should have as title 'Book Rating'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('br');
+    expect(app.title).toEqual('Book Rating');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should render title with text "Book Rating!"', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to br!');
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('Book Rating!');
   }));
 });
