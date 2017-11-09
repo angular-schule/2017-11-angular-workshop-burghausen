350fd145f023d703d7e05b4448fa848df0f1354d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-09 18:01:02 +0100

    e2e with screenshot

--- book-rating/e2e/rtk.e2e-spec.ts
+++ book-rating/e2e/rtk.e2e-spec.ts
@@ -1,5 +1,8 @@
 import { browser, element, by, $ } from 'protractor';
 
+// at the top of the test spec:
+const fs = require('fs');
+
 export class ImprintPage {
   goTo() {
     browser.get('https://www.rtkreisen.de/Impressum__2.htm');
@@ -9,21 +12,40 @@ export class ImprintPage {
     const text = $(selector);
     return text.getText();
   }
+
+  takeScreenhot(filename) {
+    browser.takeScreenshot().then((png) => {
+      this.writeScreenShot(png, filename);
+    });
+  }
+
+  // abstract writing screen shot to a file
+  private writeScreenShot(data, filename) {
+    const stream = fs.createWriteStream(filename);
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+  }
 }
 
 fdescribe('rtk website', () => {
 
   beforeAll(() => browser.waitForAngularEnabled(false));
   let page: ImprintPage;
-  beforeEach(() => page = new ImprintPage() );
+  beforeEach(() => page = new ImprintPage());
 
   it('should show the correct year on imprint page', () => {
-    const currentYear = new Date(). getFullYear();
+    const currentYear = new Date().getFullYear();
     const freiText = page.goTo().getText('.freitext');
-    expect(freiText).toContain(`© ${ currentYear } RT-Reisen GmbH`);
+    expect(freiText).toContain(`© ${currentYear} RT-Reisen GmbH`);
   });
 
-  // TODO: .eupopup-head soll "Diese Webseite verwendet Cookies." enhalten
+  it('should show the cookie popup', () => {
+    const text = page.goTo().getText('.eupopup-head');
+    expect(text).toContain(`Diese Webseite verwendet Cookies.`);
+
+    page.takeScreenhot('/screenshot.png');
+  });
+
 
   afterAll(() => browser.waitForAngularEnabled(true));
 });
