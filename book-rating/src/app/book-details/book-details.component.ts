8200bfc83c7c1b5d85d4cca5a86989e32fe6042c
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-10 15:40:23 +0100

    routing, with details: this.route.snapshot

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -1,16 +1,18 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  isbn: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.isbn = this.route.snapshot.params.isbn;
   }
-
 }
