d12bafb6874c957d5b2b9312cbf92be38e9bbc81
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-11-10 15:01:58 +0100

    routing: empty details page first

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { CreateBookComponent } from './create-book/create-book.component';
 import { BookStoreService } from './shared/book-store.service';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     BookComponent,
-    CreateBookComponent
+    CreateBookComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BookDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
